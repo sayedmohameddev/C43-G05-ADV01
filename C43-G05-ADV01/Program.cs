a46381c5ca9838d0e825297e47577d096cd58312
sayed123 <sayedmohameddev@gmai.com>
2025-01-28 22:11:09 +0200

    Q-03

--- C43-G05-ADV01/Program.cs
+++ C43-G05-ADV01/Program.cs
@@ -3,74 +3,91 @@ using System.Collections.Generic;
 
 namespace C43_G05_ADV01
 {
-    #region Question 1
+    //#region Question 1
 
     
-        #region Requirement 1: Define the Range<T> class
-        public class Range<T> where T : IComparable<T>
-        {
-            #region Requirement 2: Fields and Constructor
-            private T _min;
-            private T _max;
+    //    #region Requirement 1: Define the Range<T> class
+    //    public class Range<T> where T : IComparable<T>
+    //    {
+    //        #region Requirement 2: Fields and Constructor
+    //        private T _min;
+    //        private T _max;
 
-            public Range(T min, T max)
-            {
-                if (min.CompareTo(max) > 0)
-                {
-                    throw new ArgumentException("Minimum value cannot be greater than maximum value.");
-                }
+    //        public Range(T min, T max)
+    //        {
+    //            if (min.CompareTo(max) > 0)
+    //            {
+    //                throw new ArgumentException("Minimum value cannot be greater than maximum value.");
+    //            }
 
-                _min = min;
-                _max = max;
-            }
-            #endregion
+    //            _min = min;
+    //            _max = max;
+    //        }
+    //        #endregion
 
-            #region Requirement 3: IsInRange(T value)
-            public bool IsInRange(T value)
-            {
-                return value.CompareTo(_min) >= 0 && value.CompareTo(_max) <= 0;
-            }
-            #endregion
+    //        #region Requirement 3: IsInRange(T value)
+    //        public bool IsInRange(T value)
+    //        {
+    //            return value.CompareTo(_min) >= 0 && value.CompareTo(_max) <= 0;
+    //        }
+    //        #endregion
 
-            #region Requirement 4: Length()
-            public T Length()
-            {
-                dynamic min = _min;
-                dynamic max = _max;
-                return max - min;
-            }
-            #endregion
+    //        #region Requirement 4: Length()
+    //        public T Length()
+    //        {
+    //            dynamic min = _min;
+    //            dynamic max = _max;
+    //            return max - min;
+    //        }
+    //        #endregion
 
-            #region Requirement 5: Properties for Min and Max
-            public T Min => _min;
-            public T Max => _max;
-            #endregion
-        }
-    #endregion
+    //        #region Requirement 5: Properties for Min and Max
+    //        public T Min => _min;
+    //        public T Max => _max;
+    //        #endregion
+    //    }
+    //#endregion
 
 
-    #endregion
+    //#endregion
 
     
    
    
     internal class Program
     {
-        #region Question 2
-        public static void ReverseArrayList<T>(List<T> list)
+        //#region Question 2
+        //public static void ReverseArrayList<T>(List<T> list)
+        //{
+        //    int left = 0;
+        //    int right = list.Count - 1;
+
+        //    while (left < right)
+        //    {
+        //        T temp = list[left];
+        //        list[left] = list[right];
+        //        list[right] = temp;
+
+        //        left++;
+        //        right--;
+        //    }
+        //}
+        //#endregion
+
+        #region Question 3
+        public static List<int> GetEvenNumbers(List<int> numbers)
         {
-            int left = 0;
-            int right = list.Count - 1;
+            List<int> evenNumbers = new List<int>();
 
-            while (left < right)
+            foreach (int number in numbers)
             {
-                T temp = list[left];
-                list[left] = list[right];
-                list[right] = temp;
-
-                left++;
-                right--;
+                if (number % 2 == 0)
+                {
+                    evenNumbers.Add(number);
+                }
             }
+
+            return evenNumbers;
         }
         #endregion
         static void Main(string[] args)
@@ -86,38 +103,55 @@ namespace C43_G05_ADV01
             //Console.WriteLine($"Length of range: {range.Length()}");
 
 
-            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            //List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
 
-            Console.WriteLine("Original List (Int):");
-            for (int i = 0; i < numbers.Count; i++)
+            //Console.WriteLine("Original List (Int):");
+            //for (int i = 0; i < numbers.Count; i++)
+            //{
+            //    Console.Write(numbers[i] + " ");
+            //}
+
+            //ReverseArrayList(numbers);
+
+            //Console.WriteLine("\nReversed List (Int):");
+            //for (int i = 0; i < numbers.Count; i++)
+            //{
+            //    Console.Write(numbers[i] + " ");
+            //}
+
+            //Console.WriteLine(); 
+
+            //List<string> words = new List<string> { "Hello", "World", "C#", "Generics" };
+
+            //Console.WriteLine("Original List (String):");
+            //for (int i = 0; i < words.Count; i++)
+            //{
+            //    Console.Write(words[i] + " ");
+            //}
+
+            //ReverseArrayList(words);
+
+            //Console.WriteLine("\nReversed List (String):");
+            //for (int i = 0; i < words.Count; i++)
+            //{
+            //    Console.Write(words[i] + " ");
+            //}
+
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("Original List:");
+            foreach (int number in numbers)
             {
-                Console.Write(numbers[i] + " ");
+                Console.Write(number + " ");
             }
 
-            ReverseArrayList(numbers);
+            List<int> evenNumbers = GetEvenNumbers(numbers);
 
-            Console.WriteLine("\nReversed List (Int):");
-            for (int i = 0; i < numbers.Count; i++)
+            Console.WriteLine("\nEven Numbers List:");
+            foreach (int even in evenNumbers)
             {
-                Console.Write(numbers[i] + " ");
-            }
-
-            Console.WriteLine(); 
-
-            List<string> words = new List<string> { "Hello", "World", "C#", "Generics" };
-
-            Console.WriteLine("Original List (String):");
-            for (int i = 0; i < words.Count; i++)
-            {
-                Console.Write(words[i] + " ");
-            }
-
-            ReverseArrayList(words);
-
-            Console.WriteLine("\nReversed List (String):");
-            for (int i = 0; i < words.Count; i++)
-            {
-                Console.Write(words[i] + " ");
+                Console.Write(even + " ");
             }
 
 
